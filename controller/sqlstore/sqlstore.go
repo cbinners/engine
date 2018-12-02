e0b3af42d9dab44d3f4cd13830e4cbf422f82404
Daniel Steuernol <dlsteuer@gmail.com>
2018-12-02 10:50:40 -0800

    add database/sql limits

--- controller/sqlstore/sqlstore.go
+++ controller/sqlstore/sqlstore.go
@@ -44,6 +44,9 @@ func NewSQLStore(url string) (*Store, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
+	db.SetMaxOpenConns(50)
+	db.SetMaxIdleConns(10)
+
 	if err = db.PingContext(ctx); err != nil {
 		return nil, err
 	}
